cb34ec4421272ce9befa9416ae9c2e0bdc0e4255
bhavanisollu <51796920+bhavanisollu@users.noreply.github.com>
2021-11-22 22:27:18 +0530

    Update App.java

--- src/main/java/com/lti/jenkinsdeploy/App.java
+++ src/main/java/com/lti/jenkinsdeploy/App.java
@@ -9,5 +9,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        sleep(400);
+        System.out.println( "1st" );
+        sleep(400);
+        System.out.println( "2nd" );
+        sleep(400);
+        System.out.println( "Hello Again!!" );
     }
 }
