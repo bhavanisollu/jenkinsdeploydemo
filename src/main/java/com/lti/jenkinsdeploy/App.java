f8331d5043c192308d73f9a4104217a34b54f4f3
bhavanisollu <51796920+bhavanisollu@users.noreply.github.com>
2021-11-23 11:19:49 +0530

    Update App.java

--- src/main/java/com/lti/jenkinsdeploy/App.java
+++ src/main/java/com/lti/jenkinsdeploy/App.java
@@ -1,21 +1,43 @@
 package com.lti.jenkinsdeploy;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    public void addition(int a , int b){
+        int res = a + b;
+        System.out.println("a + b is "+res);
+    }
+    
+    public void Subtraction(int a , int b){
+        int res = a - b;
+        System.out.println("a - b is "+res);
+    }
+    
+    public void multipication(int a , int b){
+        double res = a * b;
+        System.out.println("a * b is "+res);
+    }
+    
+    public void division(int a , int b){
+        double quo = a / b;
+        double rem = a % b;
+        System.out.println("Quotient is "+quo);
+        System.out.println("Remainder is "+rem);
+    }
+    
     public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        int a = 10;
-        int b = 20;
-        int c = a+b;
-        System.out.println( "1st" );
-        System.out.println("a+b is " + c);
-        System.out.println( "2nd" );
-        
+    {   
+        Scanner sc = new Scanner(system.in);
+        System.out.println( "Enter 1st no : " );
+        int a = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Enter 2nd no : ");
+        int b = sc.nextInt();
         System.out.println( "Hello Again!!" );
     }
 }
